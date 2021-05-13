b9b945e5028854bc1f481b78fb7e1c0c5bd608f8
simonbrain <simon.brain@outlook.com>
2021-05-13 09:50:14 +0100

    Auto Dev Push

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ const info = require("./routes/info");
 app1.use("/data",data);
 app1.use("/info",info);
 
-const { auth, requiresAuth } = require('express-openid-connect');
-app1.use(
-  auth({
-    authRequired: false,
-    auth0Logout: true,
-    issuerBaseURL: 'https://dev-2k06-6q4.us.auth0.com',
-    baseURL: 'http://34.89.4.53:8000',
+//const { auth, requiresAuth } = require('express-openid-connect');
+//app1.use(
+  //auth({
+    //authRequired: false,
+    //auth0Logout: true,
+    //issuerBaseURL: 'https://dev-2k06-6q4.us.auth0.com',
+    //baseURL: 'http://34.89.4.53:8000',
     //baseURL: 'http://localhost:8000',
-    clientID: 'bM0Tl72lomPAbV3YWeXTlmGTb8tMxmpH',
-    secret: 'jkdjhbdwfjbhbekqwjbfjkbjhfbhjberjhjhlrrhjlehjlrbbljkvfrblkjbljkvkbljvef'
-  })
-);
+    //clientID: 'bM0Tl72lomPAbV3YWeXTlmGTb8tMxmpH',
+    //secret: 'jkdjhbdwfjbhbekqwjbfjkbjhfbhjberjhjhlrrhjlehjlrbbljkvfrblkjbljkvkbljvef'
+  //})
+//);
 
 //app.get('/', (req, res) => {
 app1.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out')
+    //res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out')
     //res.sendFile(path.join(__dirname, '/index.html'));
-    //res.status(200).send('Hello, Fastcharts!').end();
+    res.status(200).send('Hello from Fastcharts frontend').end();
   
 });
 
@@ -45,7 +45,7 @@ app1.listen(PORT1, () => {
 });
 
 // Start the backend server
-const PORT2 = process.env.PORT || 8001;
+const PORT2 = process.env.PORT || 8007;
 app2.listen(PORT2, () => {  
   console.log(`App listening on port ${PORT2}`);
   console.log('Press Ctrl+C to quit.');
